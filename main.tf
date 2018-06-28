5311dd6402e3ffad6dd856fdf86e4aad0445b6b0
Mackenzie Hatfield <mackenziehatfield@mackenzies-mbp.buehner-fry.com>
2018-06-28 15:34:30 -0700

    configure auto scaling group

--- main.tf
+++ main.tf
@@ -2,16 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_launch_configuration" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags {
     Name = "terraform-example"
@@ -27,13 +31,30 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
 }
 
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
