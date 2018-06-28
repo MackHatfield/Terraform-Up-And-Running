50032aefc950c057b81d0f63947837c38507fc89
Mackenzie Hatfield <mackenziehatfield@mackenzies-mbp.buehner-fry.com>
2018-06-28 14:37:20 -0700

    add variable for server port

--- main.tf
+++ main.tf
@@ -22,9 +22,15 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = ${var.server_port}
+    to_port     = ${var.server_port}
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
