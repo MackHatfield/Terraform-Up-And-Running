d47c474003ccce28a6fb3c96f14f3e733636263c
Mackenzie Hatfield <mackenziehatfield@mackenzies-mbp.buehner-fry.com>
2018-06-28 15:01:25 -0700

    fix syntax error and add variables

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              hohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags {
@@ -22,8 +22,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = ${var.server_port}
-    to_port     = ${var.server_port}
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -34,3 +34,6 @@ variable "server_port" {
   default = 8080
 }
 
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
