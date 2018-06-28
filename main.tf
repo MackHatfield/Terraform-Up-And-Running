07471a21bcfc55aecae358293f6ad284da0b7d26
Mackenzie Hatfield <mackenziehatfield@mackenzies-mbp.buehner-fry.com>
2018-06-28 14:00:20 -0700

    format document

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-40d28157"
-  instance_type = "t2.micro"
+  ami                    = "ami-40d28157"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
@@ -14,7 +14,7 @@ resource "aws_instance" "example" {
               EOF
 
   tags {
-   Name = "terraform-example" 
+    Name = "terraform-example"
   }
 }
 
@@ -22,11 +22,9 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
 }
-
